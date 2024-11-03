52af1a11112bd9934e972b61413aa2db4aec1e9a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2024-11-03 09:57:16 +0200

    First commit

---
+++ resources/views/livewire/hero-section.blade.php
@@ -0,0 +1,30 @@
+<section class="bg-gray-50">
+    <div class="max-w-screen-xl px-4 py-10 mx-auto lg:flex lg:h-screen lg:items-center">
+      <div class="max-w-xl mx-auto text-center">
+        <h1 class="text-3xl font-extrabold sm:text-5xl">
+          Online MarketPlace
+          <strong class="text-3xl font-extrabold text-blue-700 sm:block"> Discover quality products </strong>
+        </h1>
+  
+        <p class="mt-4 sm:text-xl/relaxed">
+          Browse our collection of high-quality products and enjoy seamless online shopping.
+        </p>
+  
+        <div class="flex flex-wrap justify-center gap-4 mt-8">
+          <a
+            class="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 rounded shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+            href="#"
+          >
+            Get Started
+          </a>
+  
+          <a
+            class="block w-full px-12 py-3 text-sm font-medium text-blue-600 rounded shadow hover:text-blue-700 focus:outline-none focus:ring active:text-blue-500 sm:w-auto"
+            href="#"
+          >
+            Learn More
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
